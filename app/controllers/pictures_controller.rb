5c067eb0c11cfcfc1f6bc20fb85ff3327ab61ffa
Ward Minnis <ward@minnis.net>
2014-08-27 15:45:35 -0400

    Working database. Accepting new photos

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,29 @@
+class	PicturesController < ApplicationController
+
+	def index
+			@pictures= Picture.all
+	end
+
+	def show
+		@pictures= Picture.find(params[:id])
+	end
+
+	def new
+		@picture = Picture.new
+	end
+
+	def create
+		# render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+		@picture = Picture.new(picture_params)
+		if @picture.save
+			redirect_to pictures_url
+		else
+			render :new
+		end
+	end
+
+	private
+	def picture_params
+		params.require(:picture).permit(:artist, :title, :url)
+	end
+end
