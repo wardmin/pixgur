a41163271d4218ed1c14cca412b51e3e5760187f
Ward Minnis <ward@minnis.net>
2015-01-09 22:32:59 -0500

    getting the aws s3 bucket to work

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,7 +2,8 @@ class Picture < ActiveRecord::Base
 	scope :newest_first, -> { order("created_at DESC") }
 	scope :most_recent_five, -> { newest_first.limit(5) }
 	scope :created_before, ->(time) { where("created_at < ?", time) }
-	has_attached_file :pixfile, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :pixfile, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :storage => :s3,
+                    :s3_credentials => "#{Rails.root}/config/aws.yml"
   	validates_attachment_content_type :pixfile, :content_type => /\Aimage\/.*\Z/
 
 	def self.created_before(time)
