e4fe5072bc43af7fc18d45faeea121c6f2d8b0f6
Ward Minnis <ward@minnis.net>
2015-01-10 14:12:34 -0500

    added a panel to the index page and some s3 settings

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,7 +3,7 @@ class Picture < ActiveRecord::Base
 	scope :most_recent_five, -> { newest_first.limit(5) }
 	scope :created_before, ->(time) { where("created_at < ?", time) }
 	has_attached_file :pixfile, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :storage => :s3,
-                    :s3_credentials => "#{Rails.root}/config/aws.yml"
+                    :s3_credentials => "#{Rails.root}/config/aws.yml", :s3_permissions => :public_read_write, :path => "/images/:id/:filename"
   	validates_attachment_content_type :pixfile, :content_type => /\Aimage\/.*\Z/
 
 	def self.created_before(time)
